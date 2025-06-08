ed2d52ffce889974bde58afaa194305cd2b7f8ba
23Mariya <mariyakounain644@gmail.com>
2025-06-08 12:40:02 +0530

    Initial commit

---
+++ src/main/java/com/program2/example/program2_example_jar/App.java
@@ -0,0 +1,10 @@
+package com.program2.example.program2_example_jar;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
